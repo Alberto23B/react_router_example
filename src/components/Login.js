c7174e2f1244683d16128bca05cfa261a99b9121
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2024-09-12 11:51:18 +0200

    add useNavigate hook to Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import AuthConsumer from "../hooks/auth";
 
 export default function Login() {
     const [authed, dispatch] = AuthConsumer();
-    console.log(authed);
+    let navigate = useNavigate();
+
     return (
         <>
           <h1>Login Page</h1>
           <button 
           onClick={() => {
             dispatch({type: "login"})
+            navigate("/balance")
           }}
           >Login</button> 
           <button 
           onClick={() => {
             dispatch({type: "logout"})
+            navigate("/")
           }}
           >Logout</button>
         </>
